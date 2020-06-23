ab74ff4f2bc2853f035cbc357e69aa072af33dea
amersel <amersel@bibanon.org>
2020-06-23 01:40:27 -0400

    remove unnecessary file

--- ayase/view/reports.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-
-from fastapi import FastAPI, Request, Response, status
-from fastapi.openapi.utils import get_openapi
-from fastapi.responses import HTMLResponse
